bedcbd024e68d6a1f72533fa859e9eedc2166482
EuJunqueira <filipe.junqueira@alunos.unis.edu.br>
2020-05-17 18:35:27 -0300

    atividadee1-4

---
+++ ProgCarimbo/Interacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgCarimbo
+{
+    interface Interacao
+    {
+        void usar();
+        void carregar(string Pcor, int Pcarga);
+        void trocarCor(string Pcor);
+        void trocarTexto(string Ptexto);
+    }
+}
---
+++ ProgCarimbo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProgCarimbo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TEXTO DO CARIMBO: ");
+            string texto1 = Console.ReadLine();
+            Console.WriteLine("QUAL A COR DO CARIMBO: ");
+            string cor1 = Console.ReadLine();
+            Console.WriteLine("QUANTAS CARGAS NO CARIMBO: ");
+            int carga1 = Convert.ToInt32(Console.ReadLine());
+            Carimbo use1 = new Carimbo(texto1);
+
+
+            use1.carregar(cor1, carga1);
+            use1.usar();
+
+        }
+    }
+}
